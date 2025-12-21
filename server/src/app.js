a3771c9242c1acb260996737d8bcc57fa8f21826
Luca Lee <kartg0019246@gmail.com>
2025-12-21 13:59:43 -0600

    fixed cors, adding patch

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000', 'https://tian-photography.pages.dev', 'https://www.tians-photography.com'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
